34edfa81ee591a0e8ee986081706177ed0eb9a2b
Vitor Carvalho <vitorblz@gmail.com>
2021-02-08 15:01:41 -0300

    server port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, {Application} from 'express';
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app: Application = express()
 
-app.listen(5000, ()=>{
-    console.log('server runnning')
+const PORT_APP = process.env.PORT || 5000;
+
+app.listen(PORT_APP, ()=>{
+    console.log(`server runnning on ${PORT_APP}`)
 })
 
 
